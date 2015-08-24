ffe02cafa2024b7db7c84a88cf5f4f8c1dab8b2e
NandaMadonsela <u11068753@tuks.co.za>
2015-08-24 14:43:05 +0200

    Update StoredProcedures - Nanda.sql

--- database/StoredProcedures - Nanda.sql
+++ database/StoredProcedures - Nanda.sql
@@ -110,3 +110,109 @@ BEGIN
 			`Sub_Letter_id` = sID;
 END // 
 DELIMITER ;
+--Employee
+
+DELIMITER//
+CREATE PROCEDURE spEmployee_Create
+(
+  IN Name VARCHAR(50),
+  IN Surname  VARCHAR(50),
+  IN  ContactNumber VARCHAR(15),
+  IN email VARCHAR(100),
+  IN Salary DECIMAL(8,2),
+  IN Active BOOLEAN,
+  IN Address_ID INT
+)
+BEGIN
+DECLARE insertId  INT;
+	INSERT 
+               INTO `Employee`
+                  (`Name`,
+                   `Surname`,
+                   `ContactNumber`,
+                   `email`,
+                   `Salary`,
+                   `Active`,
+                    `Addres_ID`
+                   )
+          VALUES
+               (Name,Surname,ContactNumber,email,Salary,true,Address_ID)
+ SET insertId = LAST_INSERT_ID();
+ SELECT insertId;
+ END//
+ DELIMITER;
+
+DELIMITER
+CREATE PROCEDURE spEmployee_Update
+(
+
+  IN Name VARCHAR(50),
+  IN Surname  VARCHAR(50),
+  IN  ContactNumber VARCHAR(15),
+  IN email VARCHAR(100),
+  IN Salary DECIMAL(8,2),
+  IN Active BOOLEAN,
+  IN Address_ID INT
+)
+
+ BEGIN
+      UPDATE
+            `Employee`
+         SET
+       `Name'= Name,
+       `Surname`= Surname,
+       `ContactNumber`= ContactNumber,
+       `email` = email,
+       `Salary`= Salary,
+       `Address_ID`= Address_ID
+WHERE 
+      `Employee_ID`= Employee_ID;
+
+END//
+DELIMITER;
+
+
+DELIMITER//
+CREATE PROCEDURE spEmployee_Delete
+(
+   IN Emp_ID INT
+)
+
+BEGIN 
+      UPDATE `Employee`
+       SET
+
+
+      `Active`= false
+
+      WHERE `Employee_ID` = emp_ID;
+END//
+DELIMITER;
+
+DELIMITER//
+CREATE PROCEDURE spEmployee_Read_Search
+     (
+       IN Name VARCHAR(50),
+       IN Surname VARCHAR(50),
+       IN RoleName VARCHAR(50)
+
+       )
+BEGIN
+   SELECT * FROM `Employee`
+WHERE `Name` Like Name,`Surname` Like Surname
+ AND `RoleName` Like Name
+
+ORDER BY `Name`;
+END//
+DELIMITER;
+
+CREATE PROCEDURE spEmployee_Read_ID
+(
+  IN id INT
+
+)
+BEGIN 
+SELECT *FROM `Employee`
+WHERE `Employee_ID`= id;
+END//
+DELIMITER;
